0bf60dcb7e100ad8d29ff0e27f1899f61c2206ee
alexdotzip <ajpurpora@gmail.com>
2020-07-31 03:11:21 -0400

    Player Animations Added

--- Assets/Scripts/CharacterMovement.cs
+++ Assets/Scripts/CharacterMovement.cs
@@ -6,6 +6,8 @@ public class CharacterMovement : MonoBehaviour
 {
 
     private Rigidbody2D playerRigidbody2D;
+    private Animator playerAnim;
+    private SpriteRenderer playerSpriteImage;
 
     private float movePlayerHorizontal;
     private float movePlayerVertical;
@@ -16,6 +18,8 @@ public class CharacterMovement : MonoBehaviour
     public void Awake()
     {
         playerRigidbody2D = (Rigidbody2D)GetComponent(typeof(Rigidbody2D));
+        playerAnim = (Animator)GetComponent(typeof(Animator));
+        playerSpriteImage = (SpriteRenderer)GetComponent(typeof(SpriteRenderer));
     }
     // Start is called before the first frame update
     void Start()
@@ -26,10 +30,56 @@ public class CharacterMovement : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+
         movePlayerHorizontal = Input.GetAxis("Horizontal");
         movePlayerVertical = Input.GetAxis("Vertical");
         movement = new Vector2(movePlayerHorizontal, movePlayerVertical);
 
         playerRigidbody2D.velocity = movement * speed;
+
+
+        if (movePlayerVertical!=0)
+        {
+            playerAnim.SetBool("xMove", false);
+            playerSpriteImage.flipX = false;
+
+            if (movePlayerVertical > 0)
+            {
+                playerAnim.SetInteger("yMove", 1);
+            }
+            else if (movePlayerVertical < 0)
+            {
+                playerAnim.SetInteger("yMove", -1);
+            }
+        }else
+        {
+            playerAnim.SetInteger("yMove", 0);
+            if(movePlayerHorizontal > 0)
+            {
+                playerAnim.SetBool("xMove", true);
+                playerSpriteImage.flipX = false;
+            }else if(movePlayerHorizontal < 0)
+            {
+                playerAnim.SetBool("xMove", true);
+                playerSpriteImage.flipX = true;
+            } else
+            {
+                playerAnim.SetBool("xMove", false);
+            }
+        }
+
+
+
+        if (movePlayerVertical == 0 && movePlayerHorizontal == 0)
+        {
+            playerAnim.SetBool("moving", false);
+
+        }
+        else
+        {
+            playerAnim.SetBool("moving", true);
+
+        }
+
     }
 }
