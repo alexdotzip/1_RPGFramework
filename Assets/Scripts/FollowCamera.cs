b2ff8761549f39ed473d8fc39d134bb74ad81622
alexdotzip <ajpurpora@gmail.com>
2020-08-04 18:20:41 -0400

    border final

---
+++ Assets/Scripts/FollowCamera.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FollowCamera : MonoBehaviour
+{
+
+    //Distance between player and camera horizontally
+    public float xOffset = 0f;
+    //Distance between player and camera vertically
+    public float yOffset = 0f;
+
+    public Transform player;
+
+    private void LateUpdate()
+    {
+        this.transform.position = new Vector3(player.transform.position.x + xOffset, this.transform.position.y + yOffset, -10);
+    }
+}
